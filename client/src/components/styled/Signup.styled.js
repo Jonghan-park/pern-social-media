1e0fafe7e0885ee3a7fcd92c7281db4eb1195227
Jonghan Park <jonghan.park@edu.sait.ca>
2023-01-23 08:33:32 -0700

    Update client

--- client/src/components/styled/Signup.styled.js
+++ client/src/components/styled/Signup.styled.js
@@ -3,6 +3,15 @@ import styled from "styled-components";
 const Signup = styled.div`
   height: 100vh;
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > form {
+    background-color: ${({ theme }) => theme.elevation_1};
+    border-radius: 5px;
+    height: 400px;
+    width: 90%;
+  }
 `;
 
 export const SignupButton = styled.button``;
